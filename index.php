3a60553bd71372aa6f1291c28bacc66e81fc204d
n.rivier <n.rivier@algo-factory.com>
2024-02-01 08:16:57 +0100

    ajustement

--- index.php
+++ index.php
@@ -2,13 +2,27 @@
     require 'header.php';
     require 'oeuvres.php';
 ?>
+
+<?php
+    try{
+        $artworksBDD = new PDO('mysql:host=projet_4_Php_db;dbname=coursPhp;charset=utf8mb4', 'root', 'root');
+    }catch (Exception $e){
+        die('Erreur: ' . $e->getMessage());
+    }
+?>
+<?php
+    $artworkStatement = $artworksBDD->prepare('SELECT * FROM Oeuvres');
+    $artworkStatement->execute();
+    $artworks = $artworkStatement->fetchAll();
+?>
+
 <div id="liste-oeuvres">
-    <?php foreach($oeuvres as $oeuvre): ?>
+    <?php foreach($artworks as $artwork): ?>
         <article class="oeuvre">
-            <a href="oeuvre.php?id=<?= $oeuvre['id'] ?>">
-                <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
-                <h2><?= $oeuvre['titre'] ?></h2>
-                <p class="description"><?= $oeuvre['artiste'] ?></p>
+            <a href="oeuvre.php?id=<?= $artwork['artwork_id'] ?>">
+                <img src="<?= $artwork['photo_link'] ?>" alt="<?= $artwork['artwork_name'] ?>">
+                <h2><?= $artwork['artwork_name'] ?></h2>
+                <p class="description"><?= $artwork['author_name'] ?></p>
             </a>
         </article>
     <?php endforeach; ?>
--- oeuvre.php
+++ oeuvre.php
@@ -1,38 +1,50 @@
 <?php
     require 'header.php';
     require 'oeuvres.php';
-
+?>
+<?php
+    try{
+        $artworksBDD = new PDO('mysql:host=projet_4_Php_db;dbname=coursPhp;charset=utf8mb4', 'root', 'root');
+    }catch (Exception $e){
+        die('Erreur: ' . $e->getMessage());
+    }
+?>
+<?php
+    $artworkStatement = $artworksBDD -> prepare('SELECT * FROM Oeuvres');
+    $artworkStatement->execute();
+    $artworks = $artworkStatement->fetchAll();
+?>
+<?php
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
     }
 
-    $oeuvre = null;
+    $displayArtwork = null;
 
     // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
+    foreach($artworks as $artwork) {
         // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
+        if($artwork['artwork_id'] === intval($_GET['id'])) {
+	        $displayArtwork = $artwork;
             break; // On stoppe le foreach si on a trouvé l'oeuvre
         }
     }
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
+    if(is_null($artworks)) {
         header('Location: index.php');
     }
 ?>
-
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= $displayArtwork['photo_link'] ?>" alt="<?= $displayArtwork['artwork_name'] ?>">
     </div>
     <div id="contenu-oeuvre">
-        <h1><?= $oeuvre['titre'] ?></h1>
-        <p class="description"><?= $oeuvre['artiste'] ?></p>
+        <h1><?= $displayArtwork['artwork_name'] ?></h1>
+        <p class="description"><?= $displayArtwork['author_name'] ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+             <?= $displayArtwork['artwork_description'] ?>
         </p>
     </div>
 </article>
